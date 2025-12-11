9bb02ebafa080fba7ec7dee5cdbef988488e3d54
MrOpoku1 <asedaopoku3@gmail.com>
2025-12-11 14:52:40 -0500

    Added cart activity Xml, started implementing pepperoni activity code.

--- app/src/main/java/com/example/pokuspizza/PepperoniActivity.java
+++ app/src/main/java/com/example/pokuspizza/PepperoniActivity.java
@@ -7,12 +7,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.CheckBox;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class PepperoniActivity extends AppCompatActivity {
 
+    CheckBox ExtraPepperoni, ExtraCheese, Sausage, Olives;
+    Button Add;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ExtraPepperoni = (CheckBox)findViewById(R.id.ExtraPepperoni);
+        ExtraCheese = (CheckBox)findViewById(R.id.ExtraCheese);
+        Sausage = (CheckBox)findViewById(R.id.Sausage);
+        Olives = (CheckBox)findViewById(R.id.Olives);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pepperoni);
-    }
+
+
+
+        ExtraPepperoni.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            double total=10;
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    total += 1.50;
+                    Toast.makeText(, "Extra pepperoni added!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Remove the cost
+                    total -= 1.50;
+                    Toast.makeText(this, "Extra pepperoni removed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });    }
+
 }
